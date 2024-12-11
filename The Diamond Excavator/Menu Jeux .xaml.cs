934cf57261de922b1352926ee0df22f5a9bf573b
Matt6013 <matt.barbier@etu.univ-smb.fr>
2024-12-11 09:15:56 +0100

    Menu Window

---
+++ The Diamond Excavator/Menu Jeux .xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace The_Diamond_Excavator
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu_Jeux.xaml
+    /// </summary>
+    public partial class Menu_Jeux : Window
+    {
+        public Menu_Jeux()
+        {
+            InitializeComponent();
+        }
+
+        private void butJouer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butQuit_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
